d5229239ca97e0b69d2630b8905b113ab11cbbb3
kushwahramkumar2003 <kushwahramkumar2003@gmail.com>
2024-04-15 21:48:54 +0530

    routes updated

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,19 +1,23 @@
 import express from "express";
 
-import { authGuard } from "../middlewares/authMiddleware.js";
+import { authGuard, adminGuard } from "../middlewares/authMiddleware.js";
 import {
   loginUser,
   registerUser,
   userProfile,
   updateProfile,
   updateProfilePicture,
+  getAllUsers,
+  deleteUser,
 } from "../controllers/userControllers.js";
 const router = express.Router();
 
 router.post("/register", registerUser);
 router.post("/login", loginUser);
 router.get("/profile", authGuard, userProfile);
-router.put("/updateProfile", authGuard, updateProfile);
+router.put("/updateProfile/:userId", authGuard, updateProfile);
 router.put("/updateProfilePicture", authGuard, updateProfilePicture);
+router.get("/", authGuard, adminGuard, getAllUsers);
+router.delete("/:userId", authGuard, adminGuard, deleteUser);
 
 export default router;
